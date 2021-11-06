45c1a6b95b02da22c9623ecb7271eee2238d1baf
WhiteLotus <jhonatanarroyo97@outlook.com>
2021-11-05 19:04:18 -0500

    main obsoluted changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from libsx.initTerminal import add, div, helloWorld, kill, quad, quadMax, subAdd
 import getpass
 import platform
+import re 
 
 username = getpass.getuser()
 print("Compiling Prototype v0.1")
@@ -18,12 +19,29 @@ while True:
     if (Text == "kill()"):
         b = kill()
     if (Text == "add()"):
-        b = add()
+        add_list= [float(s) for s in re.findall(r'-?\d+\.?\d*', Text)]
+        _valx = add_list[0]
+        _valy = add_list[1]
+        c = add(_valx, _valy)
     if (Text == "subAdd()"):
-        b = subAdd()
+        add_list= [float(s) for s in re.findall(r'-?\d+\.?\d*', Text)]
+        _valx = add_list[0]
+        _valy = add_list[1]
+        d = subAdd(_valx, _valy)
     if (Text == "quad()"):
-        b = quad()
+        add_list= [float(s) for s in re.findall(r'-?\d+\.?\d*', Text)]
+        _valx = add_list[0]
+        _valy = add_list[1]
+        e = quad(_valx, _valy)
     if (Text == "div()"):
-        b = div()
+        add_list= [float(s) for s in re.findall(r'-?\d+\.?\d*', Text)]
+        _valx = add_list[0]
+        _valy = add_list[1]
+        f = div(_valx, _valy)
     if (Text == "quadMax()"):
-        b = quadMax()                                
+        add_list= [float(s) for s in re.findall(r'-?\d+\.?\d*', Text)]
+        _valx = add_list[0]
+        _valy = add_list[1]
+        g = quadMax(_valx, _valy)                             
+
+
